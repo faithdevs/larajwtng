c9f14b45ab791b74332a2afd5461e715d878e945
Abraham Ugbeshe <abrahamugbeshe@gmail.com>
2020-09-01 23:58:00 +0100

    Added expection handler

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+
+Route::post('login', 'ApiController@login');
+Route::post('register', 'ApiController@register');
+
+Route::group(['middleware' => 'auth.jwt'], function () {
+    Route::get('logout', 'ApiController@logout');
+
+    Route::get('tasks', 'TaskController@index');
+    Route::get('tasks/{id}', 'TaskController@show');
+    Route::post('tasks', 'TaskController@store');
+    Route::put('tasks/{id}', 'TaskController@update');
+    Route::delete('tasks/{id}', 'TaskController@destroy');
+});
